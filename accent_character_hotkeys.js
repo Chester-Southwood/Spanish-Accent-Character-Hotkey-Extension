8abbaeae97ba3899df99aa9b6c4b00562fafd39c
Chester Southwood <chestersouthwood@gmail.com>
2022-08-27 04:43:33 -0600

    Simple application that works with Duolingo and other sites

---
+++ accent_character_hotkeys.js
@@ -0,0 +1,34 @@
+const accentCharacters = ['Á','É','Í','Ó','Ú','Ñ','á','é','í','ó','ú','ñ','¿','¡'], 
+      nonAccentCharacters = ['A','E','I','O','U','N','a','e','i','o','u','n','?','!'];
+
+var inputElem, isAccentOn = false;
+
+window.addEventListener("keydown", function(event) {
+  if ((event.ctrlKey || event.altKey || event.key === 'Tab') || (event.shiftKey && !nonAccentCharacters.includes(event.key))) return;
+
+  inputElem = getFirstInputTypeSelector();
+  
+  if (nonAccentCharacters.includes(event.key) && this.window.getSelection().toString().length === 0) {
+    event.preventDefault();
+
+    if (isAccentOn) return;
+
+    if(event.repeat) {
+      isAccentOn = true;
+      inputElem.value = inputElem.value.substring(0, inputElem.value.length - 1) + accentCharacters[nonAccentCharacters.indexOf(event.key)];
+    } else {
+      inputElem.value += event.key;
+      inputElem.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  }
+});
+
+window.addEventListener("keyup", (event) => isAccentOn = false, true);
+
+function getFirstInputTypeSelector() {
+  console.log(document.activeElement.tagName);
+  if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') return document.activeElement;
+  
+  const inputSelector = document.querySelector('INPUT');
+  return inputSelector === null ? document.querySelector('TEXTAREA') : inputSelector;
+}
